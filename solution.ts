046b0ffd2cbf306a8f215dd59338f32daef30188
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-11-18 02:07:57 +0600

    Problems 1 solved

--- solution.ts
+++ solution.ts
@@ -1,1 +1,18 @@
 // Solution All problems
+
+//Solution 01 
+
+type inputType = string | number | boolean;
+
+const formatValue = (input: inputType): inputType => {
+  if (typeof input === "string") {
+    return input.toUpperCase();
+  } 
+  if (typeof input === "number") {
+    return input * 10;
+  } 
+  if (typeof input === "boolean") {
+    return !input;
+  }
+    return input;
+};
